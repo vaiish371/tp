aa1fef0d76d0b694725732096a91f56c9b7f3f64
jianningzhuang <jianning.personal@gmail.com>
2021-03-11 14:43:09 +0800

    fix gradlew

--- src/main/java/seedu/duke/Assignment.java
+++ src/main/java/seedu/duke/Assignment.java
@@ -2,27 +2,28 @@ package seedu.duke;
 
 public class Assignment {
     private String name;
-//    private String deadline;
-//    private float weightage;
+    //    private String deadline;
+    //    private float weightage;
+    
     public Assignment(String name) {
         this.name = name;
     }
 
-//    public String getDeadline() {
-//        return deadline;
-//    }
-//
-//    public void setDeadline(String deadline) {
-//        this.deadline = deadline;
-//    }
-//
-//    public float getWeightage() {
-//        return weightage;
-//    }
-//
-//    public void setWeightage(float weightage) {
-//        this.weightage = weightage;
-//    }
+    //    public String getDeadline() {
+    //        return deadline;
+    //    }
+    //
+    //    public void setDeadline(String deadline) {
+    //        this.deadline = deadline;
+    //    }
+    //
+    //    public float getWeightage() {
+    //        return weightage;
+    //    }
+    //
+    //    public void setWeightage(float weightage) {
+    //        this.weightage = weightage;
+    //    }
 
     @Override
     public String toString() {
