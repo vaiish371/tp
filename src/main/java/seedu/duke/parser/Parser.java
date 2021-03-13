84a74196d4646cdc61875710c79956f75090008a
jianningzhuang <jianning.personal@gmail.com>
2021-03-13 21:10:59 +0800

    enable assertions and add logging to Parser

--- src/main/java/seedu/duke/parser/Parser.java
+++ src/main/java/seedu/duke/parser/Parser.java
@@ -8,25 +8,36 @@ import seedu.duke.command.ListModuleAssignmentsCommand;
 import seedu.duke.exception.InvalidCommandException;
 import seedu.duke.exception.ModManException;
 
+import java.util.logging.Logger;
+import java.util.logging.Level;
+
 public class Parser {
+    private static Logger logger = Logger.getLogger(Parser.class.getName());
 
     public static Command parse(String line) throws ModManException {
+        logger.setLevel(Level.INFO);
+        logger.log(Level.FINE, "Parsing user command");
         Command command = null;
         String[] words = line.split(" ");
         switch (words[0]) {
         case "bye":
+            logger.log(Level.INFO, "bye command entered");
             command = new ExitCommand();
             break;
         case "addmodule":
+            logger.log(Level.INFO, "addmodule command entered");
             command = new AddModuleCommand(words[1]);
             break;
         case "addassignment":
+            logger.log(Level.INFO, "addassignment command entered");
             command = new AddAssignmentCommand(words[1], words[2]);
             break;
         case "listassignment":
+            logger.log(Level.INFO, "listassignment command entered");
             command = new ListModuleAssignmentsCommand(words[1]);
             break;
         default:
+            logger.log(Level.WARNING, "Invalid command entered");
             throw new InvalidCommandException();
         }
         return command;
