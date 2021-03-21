e39b422c42ae001270aaa6d4ee06b9cc56f220f5
vvvvh123 <74040892+vvvvh123@users.noreply.github.com>
2021-03-21 18:56:58 +0800

    Update ModManCommandTest.java to fix failing CI

--- src/test/java/seedu/duke/command/AddModuleCommandTest.java
+++ src/test/java/seedu/duke/command/AddModuleCommandTest.java
@@ -15,7 +15,7 @@ class AddModuleCommandTest {
     public static Ui ui = new Ui();
 
     @Test
-    void addmodule_CS2113T_success() throws ModuleNotFoundException, AssignmentNotFoundException {
+    void addmodule_CS2113T_success() throws ModManException {
         Command c = new AddModuleCommand("CS2113T");
         c.execute(data, ui);
         assertEquals("CS2113T", data.find("CS2113T").getModuleCode());
