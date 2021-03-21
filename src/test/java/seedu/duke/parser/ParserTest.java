e8ffabd4dffd50cdc19b3a384842d21e8bfe9009
vvvvh123 <74040892+vvvvh123@users.noreply.github.com>
2021-03-21 19:18:22 +0800

    Fix checkstyle

--- src/test/java/seedu/duke/parser/ParserTest.java
+++ src/test/java/seedu/duke/parser/ParserTest.java
@@ -50,30 +50,30 @@ class ParserTest {
         assertEquals(true, c instanceof AddAssignmentCommand);
     }
 
-//     @Test
-//     void testParse_addAssignmentMissingParam_InvalidCommandException() {
-//         Parser parser = new Parser();
-//         String inputString = "add assignment /m CS2113T";
-//         assertThrows(InvalidCommandException.class, () -> {
-//             parser.parse(inputString);
-//         });
-//     }
+    //     @Test
+    //     void testParse_addAssignmentMissingParam_InvalidCommandException() {
+    //         Parser parser = new Parser();
+    //         String inputString = "add assignment /m CS2113T";
+    //         assertThrows(InvalidCommandException.class, () -> {
+    //             parser.parse(inputString);
+    //         });
+    //     }
 
-//     @Test
-//     void testParse_addAssignmentEmptyAssignment_InvalidCommandException() {
-//         Parser parser = new Parser();
-//         String inputString = "add assignment /m CS2113T /a     ";
-//         assertThrows(InvalidCommandException.class, () -> {
-//             parser.parse(inputString);
-//         });
-//     }
+    //     @Test
+    //     void testParse_addAssignmentEmptyAssignment_InvalidCommandException() {
+    //         Parser parser = new Parser();
+    //         String inputString = "add assignment /m CS2113T /a     ";
+    //         assertThrows(InvalidCommandException.class, () -> {
+    //             parser.parse(inputString);
+    //         });
+    //     }
 
-//     @Test
-//     void testParse_listAssignmentValid_ListModuleAssignmentsCommand() throws ModManException {
-//         Parser parser = new Parser();
-//         String inputString = "list assignment CS2113T";
-//         Command c = parser.parse(inputString);
-//         assertEquals(true, c instanceof ListModuleAssignmentsCommand);
+    //     @Test
+    //     void testParse_listAssignmentValid_ListModuleAssignmentsCommand() throws ModManException {
+    //         Parser parser = new Parser();
+    //         String inputString = "list assignment CS2113T";
+    //         Command c = parser.parse(inputString);
+    //         assertEquals(true, c instanceof ListModuleAssignmentsCommand);
     //     }
 
     @Test
