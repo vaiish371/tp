f2d76da90ca74cd85b25089b6b73c844db7c8263
Vikasharlani <e0523376@u.nus.edu>
2021-03-23 00:25:35 +0800

    Add CurrentModuleCommand class

---
+++ src/main/java/seedu/duke/command/CurrentModuleCommand.java
@@ -0,0 +1,24 @@
+package seedu.duke.command;
+
+import seedu.duke.Module;
+import seedu.duke.data.Data;
+import seedu.duke.exception.ModuleNotFoundException;
+import seedu.duke.parser.Parser;
+import seedu.duke.ui.Ui;
+
+public class CurrentModuleCommand extends Command {
+    public String moduleCode;
+    public CurrentModuleCommand() {
+        moduleCode = Parser.getCurrentModule();
+        assert this.moduleCode != null : "Module code cannot be null";
+    }
+
+    public void execute(Data data, Ui ui) throws ModuleNotFoundException {
+        if (moduleCode.equals("null")) {
+            ui.printModuleInfo();
+        } else {
+            Module currentModule = data.find(moduleCode);
+            ui.printModuleInfo(currentModule);
+        }
+    }
+}
