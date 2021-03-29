44ac7cefd72caa5a9bc45d25d97eeec104e17df4
bryanwhl <42731945+bryanwhl@users.noreply.github.com>
2021-03-29 16:18:31 +0800

    Update Storable.java

--- src/main/java/seedu/duke/Storable.java
+++ src/main/java/seedu/duke/Storable.java
@@ -5,7 +5,6 @@ public interface Storable {
     /**
     * Interface method that mandates a class to be storable when implemented.
     * @return a String containing information about the attributes of an object to be stored in the database
-    * End each string with a \n.
     */
     public String toStorage();
 
