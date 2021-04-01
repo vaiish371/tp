03dd58a6853c0e65406ad013fed93703a38a550e
bryanwhl <bryanwhl1999@gmail.com>
2021-04-01 21:59:04 +0800

    fix duplicate assignments

--- src/main/java/seedu/duke/exception/DuplicateAssignmentException.java
+++ src/main/java/seedu/duke/exception/DuplicateAssignmentException.java
@@ -2,7 +2,7 @@ package seedu.duke.exception;
 
 public class DuplicateAssignmentException extends ModManException {
     public DuplicateAssignmentException() {
-        this.errorMessage = "This assignment already exist! Please edit the existing\n" +
-                "\t assignment or remove it before continuing.";
+        this.errorMessage = "This assignment already exist! Please edit the existing\n"
+                + "\t assignment or remove it before continuing.";
     }
 }
