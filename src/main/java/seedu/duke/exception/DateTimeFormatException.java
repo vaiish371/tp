b79ee24bf7d89c5fd94da9aa0a7ad9bdb0b323ff
bryanwhl <bryanwhl1999@gmail.com>
2021-04-10 04:00:40 +0800

    add tests for deadline and fix deadline bugs

--- src/main/java/seedu/duke/exception/DateTimeFormatException.java
+++ src/main/java/seedu/duke/exception/DateTimeFormatException.java
@@ -2,6 +2,6 @@ package seedu.duke.exception;
 
 public class DateTimeFormatException extends ModManException {
     public DateTimeFormatException() {
-        this.errorMessage = "Date/Time Format is wrong! Please try again.";
+        this.errorMessage = "Date/Time Format is wrong! Please re-enter the Date/Time in the following format: dd MM yyyy. Note that only year 2021 to 2030 is accepted.";
     }
 }
---
+++ src/test/java/seedu/duke/storage/StorageTest.java
@@ -0,0 +1,18 @@
+package seedu.duke.storage;
+import org.junit.jupiter.api.Test;
+import seedu.duke.command.AddAssignmentCommand;
+import seedu.duke.command.AddModuleCommand;
+import seedu.duke.command.Command;
+import seedu.duke.command.ExitCommand;
+import seedu.duke.command.SetAssignmentDeadlineCommand;
+import seedu.duke.exception.InsufficientParametersException;
+import seedu.duke.exception.InvalidCommandException;
+import seedu.duke.exception.ModManException;
+import seedu.duke.Storage;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class StorageTest {
+
+}
