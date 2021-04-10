44f6ae95ea9d9808388fc19ebb09a8e7b0b4519e
Vikasharlani <e0523376@u.nus.edu>
2021-04-10 11:33:11 +0800

    Fix checkstyle

--- src/test/java/seedu/duke/parser/ParserTest.java
+++ src/test/java/seedu/duke/parser/ParserTest.java
@@ -52,33 +52,33 @@ class ParserTest {
         assertEquals(true, c instanceof AddAssignmentCommand);
     }
 
-     @Test
-     void testParse_addAssignmentMissingParam_InvalidCommandException() {
-         Parser parser = new Parser();
-         String inputString = "add assignment ";
-         assertThrows(InvalidCommandException.class, () -> {
-             parser.parse(inputString);
-         });
-     }
+    @Test
+    void testParse_addAssignmentMissingParam_InvalidCommandException() {
+        Parser parser = new Parser();
+        String inputString = "add assignment ";
+        assertThrows(InvalidCommandException.class, () -> {
+            parser.parse(inputString);
+        });
+    }
 
-     @Test
-     void testParse_addAssignmentEmptyAssignment_InsufficientParametersException() {
-         Parser parser = new Parser();
-         String inputString = "add assignment /a     ";
-         assertThrows(InsufficientParametersException.class, () -> {
-             parser.parse(inputString);
-         });
-     }
+    @Test
+    void testParse_addAssignmentEmptyAssignment_InsufficientParametersException() {
+        Parser parser = new Parser();
+        String inputString = "add assignment /a     ";
+        assertThrows(InsufficientParametersException.class, () -> {
+            parser.parse(inputString);
+        });
+    }
 
-     @Test
-     void testParse_listAssignmentValid_ListModuleAssignmentsCommand() throws ModManException {
-         Parser parser = new Parser();
-         Module testModule = new Module("CS2113T");
-         Parser.setCurrentModule(testModule.getModuleCode());
-         String inputString = "list assignments";
-         Command c = parser.parse(inputString);
-         assertEquals(true, c instanceof ListModuleAssignmentsCommand);
-     }
+    @Test
+    void testParse_listAssignmentValid_ListModuleAssignmentsCommand() throws ModManException {
+        Parser parser = new Parser();
+        Module testModule = new Module("CS2113T");
+        Parser.setCurrentModule(testModule.getModuleCode());
+        String inputString = "list assignments";
+        Command c = parser.parse(inputString);
+        assertEquals(true, c instanceof ListModuleAssignmentsCommand);
+    }
 
     @Test
     void testParse_setAssignmentDeadlineValid_SetAssignmentDeadlineCommand() throws ModManException {
