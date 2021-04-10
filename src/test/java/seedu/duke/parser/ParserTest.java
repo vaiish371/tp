b90fba57683571d96952d526a40fb9c0af8b24a2
jianningzhuang <jianning.personal@gmail.com>
2021-04-10 12:17:38 +0800

    try again

--- src/test/java/seedu/duke/parser/ParserTest.java
+++ src/test/java/seedu/duke/parser/ParserTest.java
@@ -53,10 +53,28 @@ class ParserTest {
     }
 
     @Test
+    void testParse_addAssignmentMissingParam_InvalidCommandException() {
+        Parser parser = new Parser();
+        String inputString = "add assignment ";
+        assertThrows(InvalidCommandException.class, () -> {
+            parser.parse(inputString);
+        });
+    }
+
+    @Test
+    void testParse_addAssignmentEmptyAssignment_InsufficientParametersException() {
+        Parser parser = new Parser();
+        String inputString = "add assignment /a     ";
+        assertThrows(InsufficientParametersException.class, () -> {
+            parser.parse(inputString);
+        });
+    }
+
+    @Test
     void testParse_listAssignmentValid_ListModuleAssignmentsCommand() throws ModManException {
         Parser parser = new Parser();
-        String currentModule = "CS2113T";
-        parser.setCurrentModule(currentModule);
+        Module testModule = new Module("CS2113T");
+        Parser.setCurrentModule(testModule.getModuleCode());
         String inputString = "list assignments";
         Command c = parser.parse(inputString);
         assertEquals(true, c instanceof ListModuleAssignmentsCommand);
